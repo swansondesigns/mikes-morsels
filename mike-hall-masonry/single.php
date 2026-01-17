3b97a8dbbbe419a5a0e46d256656946e9967085d
Graham Swanson <graham.swanson@swansondesigns.com>
2026-01-17 13:56:34 -0700

    feat(image): restore header image
    All images have been removed from the site and from each post so this is safe to put back.

--- mike-hall-masonry/single.php
+++ mike-hall-masonry/single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Single post template
  */
@@ -6,28 +7,32 @@
 ?>
 <?php get_header(); ?>
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>     
-       
-   		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <div class="pinbin-category"><p><?php the_category(', ') ?></p></div>
-      <div class="post-nav">
-          <div class="post-prev"><?php previous_post_link('%link', '&larr;'); ?></div>
-          <div class="post-next"><?php next_post_link('%link', '&rarr;'); ?></div>
-      </div>  
-            <div class="pinbin-copy">
-                <h1><?php the_title(); ?></h1>
-                 <p class="pinbin-meta"><?php _e( 'By', 'pinbin' ); ?> <?php the_author(); ?>, <?php the_time(get_option('date_format')); ?></p>
-           		 <?php the_content('Read more'); ?> 
-	<div class="pagelink"><?php wp_link_pages(); ?></div>                
-	 <div class="posttags"><?php the_tags(); ?></div>
-                <div class="clear"></div>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-                </div>
-          
-       </div>
-       
-		<?php endwhile; endif; ?>
+		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<?php if (has_post_thumbnail()) { ?>
+				<div class="pinbin-image"><?php the_post_thumbnail('detail-image');  ?></div>
+			<?php } ?>
+			<div class="pinbin-category">
+				<p><?php the_category(', ') ?></p>
+			</div>
+			<div class="post-nav">
+				<div class="post-prev"><?php previous_post_link('%link', '&larr;'); ?></div>
+				<div class="post-next"><?php next_post_link('%link', '&rarr;'); ?></div>
+			</div>
+			<div class="pinbin-copy">
+				<h1><?php the_title(); ?></h1>
+				<p class="pinbin-meta"><?php _e('By', 'pinbin'); ?> <?php the_author(); ?>, <?php the_time(get_option('date_format')); ?></p>
+				<?php the_content('Read more'); ?>
+				<div class="pagelink"><?php wp_link_pages(); ?></div>
+				<div class="posttags"><?php the_tags(); ?></div>
+				<div class="clear"></div>
+
+			</div>
+
+		</div>
+
+<?php endwhile;
+endif; ?>
 
 <?php get_footer(); ?>
-
-
